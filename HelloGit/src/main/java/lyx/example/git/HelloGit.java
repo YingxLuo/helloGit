5b6e9e48182e49357ae6601cf31bae12212e5f3e
YingxLuo <lyx_xust@126.com>
2021-05-23 21:13:35 +0800

    更新1

--- HelloGit/src/main/java/lyx/example/git/HelloGit.java
+++ HelloGit/src/main/java/lyx/example/git/HelloGit.java
@@ -3,5 +3,6 @@ package lyx.example.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HELLO GIT!!!");
+        System.out.println("更新1!!!");
     }
 }
